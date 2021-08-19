2fab7e6f917cdf009f0206cebb14df9e27ef82ae
Transparency010101 <ossys1010110@gmail.com>
2021-08-19 15:53:35 +0300

    add comment and some JS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 console.log('Hello wolrd!');
-
+// $ Функции для прикола
+function comment() {
+    console.log('Комментировать');
+}
+function like() {
+    console.log('Лайк');
+}
+function subscripe() {
+    console.log('Подписаться');
+}
